1c226a346bf0c11adbb7fdcbaad01f41d30ffdb6
Dimo <dvelikov24@gmail.com>
2022-02-25 17:32:18 -0500

    CY-11-7 Vote for a pool test final update

--- src/test/java/com/nextbasecrm/tests/Dimo.java
+++ src/test/java/com/nextbasecrm/tests/Dimo.java
@@ -1,5 +1,6 @@
 package com.nextbasecrm.tests;
 
+import com.github.javafaker.Faker;
 import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.CRM_Utilities;
 import com.nextbasecrm.utilities.WebDriverFactory;
@@ -23,48 +24,61 @@ public class Dimo {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com/");
+        CRM_Utilities.crm_login(driver, "marketing31@cydeo.com", "UserUser");
+
     }
+
 
     @Test
     public void CY_11_7() {
 
-        // 1. Users can select one answer and click the “VOTE” button to vote for a poll.
+        //1- Create a new pool by clicking on the pool button
+        WebElement createPoolBtn = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-vote']"));
+        createPoolBtn.click();
+        BrowserUtils.sleep(2);
 
+        //2- Switch to the inner ifame and type random content text by using a Faker
+        driver.switchTo().frame(0);
+        WebElement poolContent = driver.findElement(By.xpath("//body[@contenteditable]"));
+        Faker faker = new Faker();
+        String poolContentInput = faker.programmingLanguage().name();
+        poolContent.sendKeys(poolContentInput);
 
-        driver.get("https://login2.nextbasecrm.com/");
+        //3- Switch to parent frame and enter input for pool questions and votes
+        driver.switchTo().parentFrame();
+        WebElement poolQuestion = driver.findElement(By.xpath("//input[@class='vote-block-title adda']"));
+        poolQuestion.sendKeys("Test Question");
+        WebElement poolAnswer1 = driver.findElement(By.xpath("//input[@id='answer_0__0_']"));
+        poolAnswer1.sendKeys("vote1");
+        WebElement poolAnswer2 = driver.findElement(By.xpath("//input[@id='answer_0__1_']"));
+        poolAnswer2.sendKeys("vote2");
 
-
-        CRM_Utilities.crm_login(driver, "marketing31@cydeo.com", "UserUser");
-
-
-        WebElement voteAgainBtn = driver.findElement(By.xpath("//form[@name='vote-form-nSLjLH1065']//button[@data-bx-vote-button='showVoteForm']"));
-
-        if (voteAgainBtn.isDisplayed()) {
-            voteAgainBtn.click();
-        }
-
-
-        WebElement selectOption = driver.findElement(By.xpath("//form[@name='vote-form-nSLjLH1065']//label[@for='vote_radio_1106_2492']//span"));
-
-        selectOption.click();
-
-
-        WebElement voteBtn = driver.findElement(By.xpath("//form[@name='vote-form-nSLjLH1065']//button[@class= 'ui-btn ui-btn-lg ui-btn-primary']"));
-        voteBtn.click();
-
+        //4- Click on the send button
+        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendBtn.click();
         BrowserUtils.sleep(3);
 
+
+        //5- Locate the first voting option on the pool we just create anc click on it
+        WebElement firstVoteOption = driver.findElement(By.xpath("(//span[@class='bx-vote-block-inp-substitute'])[1]"));
+        firstVoteOption.click();
+
+        //6- Click on the vote button
+        WebElement voteBtn = driver.findElement(By.xpath("(//button[@class='ui-btn ui-btn-lg ui-btn-primary'])[1]"));
+        voteBtn.click();
+        BrowserUtils.sleep(3);
+
+        //7 Assert that vote button is not displayed
         Assert.assertFalse(voteBtn.isDisplayed());
-
-        String result = driver.findElement(By.xpath("//form[@name='vote-form-nSLjLH1065']//a[@data-bx-vote-answer='2492']")).getText();
-
-        Assert.assertEquals(result, "1");
-
 
     }
 
 
 }
+
+
+
 
 
 
